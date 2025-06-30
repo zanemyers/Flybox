f826bbadc1cdfcf305f85d469dae5f9e5fb93ce1
Zane Myers <zane15myers@gmail.com>
2025-06-30 15:08:56 +0300

    re-name & organize
    - and uncomment

--- report_scraper/reportUtils.js
+++ report_scraper/reportUtils.js
@@ -112,7 +112,7 @@ async function scrapeVisibleText(page, selector) {
 
   // Evaluate the element within the browser context to check visibility and extract text
   return await element.evaluate((node) => {
-    // Retrieve computed CSS styles of the element to determine visibility
+    // Retrieve computed CSS public of the element to determine visibility
     const style = window.getComputedStyle(node);
     const isVisible =
       style.display !== "none" && style.visibility !== "hidden" && node.offsetParent !== null;
