d33423e59902ac3550f0367766de2fa164f844ce
Zane Myers <zane15myers@gmail.com>
2025-07-01 15:31:17 +0300

    Use scss
    - install stylelint and bootstrap

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "RR Tacklebox" }); // Just the name without extension, Express uses the view engine
+  res.render("index", { title: "The Flybox" }); // Just the name without extension, Express uses the view engine
 });
 
 router.get("/about", (req, res) => {
-  res.render("about"); // Render about.ejs instead of sending HTML file directly
+  res.render("about", { title: "About Flybox" }); // Render about.ejs instead of sending HTML file directly
 });
 
 export default router;
