b2e525492ae92900728a67779fc69f1ce0372d81
Zane Myers <zane15myers@gmail.com>
2025-07-01 09:46:27 +0300

    small readme update

--- shop_scraper/shopScraper.js
+++ shop_scraper/shopScraper.js
@@ -46,7 +46,7 @@ async function main() {
  * @returns {Promise<object[]>} A list of local shops, or an empty array if none found.
  */
 async function fetchShops() {
-  const cacheFile = "./assets/example_files/shops.json"; // TODO: Allow user to pass this in
+  const cacheFile = "./example_files/shops.json"; // TODO: Allow user to pass this in
   const meta = {
     query: process.env.SEARCH_QUERY,
     coordinates: `${process.env.SEARCH_LAT},${process.env.SEARCH_LONG}`,
