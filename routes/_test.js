4bbf1e0ef9cbeae1110bf6ba2012c6594311df69
Zane Myers <zane15myers@gmail.com>
2025-07-02 10:18:35 +0300

    override bootstrap primary & secondary

--- routes/_test.js
+++ routes/_test.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/error", (_req, _res, next) => {
   const err = new Error("Intentional test error");
-  err.status = 403; // or any status code you want
+  err.status = 400; // or any status code you want
   next(err);
 });
 
