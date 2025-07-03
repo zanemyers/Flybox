1ebad0e7e53284ae1aac5ca786589c368d2ee495
Zane Myers <zane15myers@gmail.com>
2025-07-03 19:14:47 +0300

    Link up form to shop-script

--- routes/_app.js
+++ routes/_app.js
@@ -2,13 +2,13 @@ import express from "express";
 
 const router = express.Router();
 router.get("/report-scraper", (req, res) => {
-  res.render("forms/report_scraper", {
+  res.render("report_scraper", {
     title: "Report Scraper",
     // Add any other context variables you want here
   });
 });
 router.get("/shop-scraper", (req, res) => {
-  res.render("forms/shop_scraper", {
+  res.render("shop_scraper", {
     title: "Shop Scraper",
   });
 });
